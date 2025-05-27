15733c7be9d0926bb020fb78fe54c3e723d275b9
Rekoyah <bludonut95@gmail.com>
2025-05-27 16:05:19 -0500

    import instead const, moodmusicdata

--- moodtunes/mood-tunes-backend/server.js
+++ moodtunes/mood-tunes-backend/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
+import express from 'express';
+import cors from 'cors';
+import { readFileSync } from 'fs';
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 
 
-const moodSongs = JSON.parse(fs.readFileSync('moodmusicdata.json', 'utf-8'));
+const moodSongs = JSON.parse(readFileSync('moodmusicdata.json', 'utf-8'));
 
 
-app.get('/songs', (req, res) => {
+app.get('/moodmusicdata', (req, res) => {
   const mood = req.query.mood;
   if (!mood || !moodSongs[mood]) {
     return res.status(404).json({ message: 'Mood not found' });

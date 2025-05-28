752d06376dabc842a1063dcd3b6d8478688b9798
Rekoyah <bludonut95@gmail.com>
2025-05-27 20:00:46 -0500

    installed cors

--- moodtunes/mood-tunes-backend/server.js
+++ moodtunes/mood-tunes-backend/server.js
@@ -1,24 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import { readFileSync } from 'fs';
-const app = express();
-const PORT = 5000;
 
+const app = express();
 app.use(cors());
 
-
-const moodSongs = JSON.parse(readFileSync('moodmusicdata.json', 'utf-8'));
-
+const songs = JSON.parse(readFileSync('./moodmusicdata.json', 'utf-8'));
 
 app.get('/moodmusicdata', (req, res) => {
   const mood = req.query.mood;
-  if (!mood || !moodSongs[mood]) {
+  if (!mood || !moodmusicdata[mood]) {
     return res.status(404).json({ message: 'Mood not found' });
   }
-  res.json(moodSongs[mood]);
+  res.json(moodmusicdata[mood]);
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
 });
